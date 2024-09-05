d991f75d82a41126a9faf72002872ef3ade0b128
Saqibi4213 <mehriamosawer@gmail.com>
2024-09-05 21:45:12 +0430

    all task passed by my side

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import Rockets from './components/Rockets';
+import MyProfile from './components/MyProfile';
+import { getDataFromServer } from './redux/Rockets/RocketsSlice';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getDataFromServer());
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="myprofile" element={<MyProfile />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
